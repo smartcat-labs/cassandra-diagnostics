252acd5557a85016bee6d7984d4674aa2881ef12
Matija Gobec <matija0204@gmail.com>
2016-06-02 22:36:49 +0200

    Fixed pom and test (#50)
    * Fixed pom and javadoc argument
    Fixed test (connection reset error) and removed sleep
    
    * Removed unused code

--- cassandra-diagnostics-connector21/src/test/java/io/smartcat/cassandra/diagnostics/connector/ConnectorImplTest.java
+++ cassandra-diagnostics-connector21/src/test/java/io/smartcat/cassandra/diagnostics/connector/ConnectorImplTest.java
@@ -1,8 +1,9 @@
 package io.smartcat.cassandra.diagnostics.connector;
 
-import java.io.IOException;
-import java.lang.instrument.Instrumentation;
-
+import com.datastax.driver.core.Cluster;
+import com.datastax.driver.core.Session;
+import io.smartcat.cassandra.diagnostics.Query;
+import io.smartcat.cassandra.utils.EmbeddedCassandraServerHelper;
 import org.apache.cassandra.exceptions.ConfigurationException;
 import org.apache.thrift.transport.TTransportException;
 import org.junit.Assert;
@@ -10,14 +11,12 @@ import org.junit.BeforeClass;
 import org.junit.Test;
 import org.springframework.instrument.InstrumentationSavingAgent;
 
-import com.datastax.driver.core.Cluster;
-import com.datastax.driver.core.Session;
-
-import io.smartcat.cassandra.diagnostics.Query;
-import io.smartcat.cassandra.utils.EmbeddedCassandraServerHelper;
+import java.io.IOException;
+import java.lang.instrument.Instrumentation;
 
 public class ConnectorImplTest {
 
+    private static Cluster cluster;
     private static Session session;
     private static boolean queryIntercepted;
 
@@ -37,30 +36,18 @@ public class ConnectorImplTest {
             }
         });
         EmbeddedCassandraServerHelper.startEmbeddedCassandra();
-        session = Cluster.builder()
-                .addContactPoint("127.0.0.1")
-                .withPort(9142)
-                .build()
-                .connect();
+        cluster = Cluster.builder().addContactPoint("127.0.0.1").withPort(9142).build();
+        session = cluster.connect();
     }
 
     @Test
     public void test() {
-        session.execute("CREATE KEYSPACE IF NOT EXISTS test_keyspace " +
-                "WITH REPLICATION = { 'class' : 'SimpleStrategy', 'replication_factor' : 1 };");
+        session.execute("CREATE KEYSPACE IF NOT EXISTS test_keyspace "
+                + "WITH REPLICATION = { 'class' : 'SimpleStrategy', 'replication_factor' : 1 };");
         session.execute("CREATE TABLE IF NOT EXISTS test_keyspace.test_table (uid uuid PRIMARY KEY);");
         session.execute("SELECT * FROM test_keyspace.test_table");
-        sleep(1000);
-        session.close();
+        cluster.close();
         Assert.assertTrue(queryIntercepted);
-    }
-
-    private static void sleep(int duration) {
-        try {
-            Thread.sleep(duration);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
     }
 
 }

5ff48f8b6a54d7ba0e78d60a3502495a3da24594
Nikola Ivancevic <nikola.ivancevic@smartcat.io>
2016-06-03 23:39:57 +0200

    Adding new unit tests for connector21 module.

---
+++ cassandra-diagnostics-connector21/src/test/java/io/smartcat/cassandra/diagnostics/connector/ConnectorImplTest.java
@@ -0,0 +1,68 @@
+package io.smartcat.cassandra.diagnostics.connector;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Matchers.any;
+import static org.mockito.Matchers.anyBoolean;
+import static org.mockito.Matchers.same;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+
+import java.lang.instrument.ClassFileTransformer;
+import java.lang.instrument.Instrumentation;
+import java.lang.reflect.Field;
+
+import org.apache.cassandra.cql3.CQLStatement;
+import org.apache.cassandra.cql3.QueryOptions;
+import org.apache.cassandra.cql3.QueryProcessor;
+import org.apache.cassandra.service.QueryState;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.springframework.instrument.InstrumentationSavingAgent;
+
+public class ConnectorImplTest {
+
+    private static Instrumentation instrumentation;
+
+    @BeforeClass
+    public static void setUp() {
+        instrumentation = InstrumentationSavingAgent.getInstrumentation();
+    }
+
+    @Test
+    public void testInitialization() {
+        QueryReporter queryReporter = mock(QueryReporter.class);
+        Instrumentation inst = mock(Instrumentation.class);
+        ConnectorImpl connector = new ConnectorImpl();
+        connector.init(inst, queryReporter);
+
+        assertThat(ConnectorImpl.queryProcessorWrapper).isInstanceOf(QueryProcessorWrapper.class);
+        verify(inst).addTransformer(any(ClassFileTransformer.class), anyBoolean());
+    }
+
+    @Test
+    public void testInterception() throws Exception {
+        Connector connector = new ConnectorImpl();
+        connector.init(instrumentation, mock(QueryReporter.class));
+        QueryProcessorWrapper queryProcessorWrapper = mock(QueryProcessorWrapper.class);
+        setStatic(ConnectorImpl.class.getDeclaredField("queryProcessorWrapper"), queryProcessorWrapper);
+
+        QueryProcessor queryProcessor = QueryProcessor.instance;
+
+        CQLStatement statement = mock(CQLStatement.class);
+        QueryState queryState = mock(QueryState.class);
+        QueryOptions options = mock(QueryOptions.class);
+        queryProcessor.processStatement(statement, queryState, options);
+
+        verify(queryProcessorWrapper).processStatement(
+                same(statement), same(queryState), same(options), any(Logger.class));
+    }
+
+    private void setStatic(Field field, Object newValue) throws Exception {
+        field.setAccessible(true);
+        Field modifiersField = Field.class.getDeclaredField("modifiers");
+        modifiersField.setAccessible(true);
+        field.set(null, newValue);
+    }
+
+}

8b80d8a27d3dc8744f01cafb8b5b0c3c0b8fb2f5
Nikola Ivancevic <ivancevic.nikola@gmail.com>
2016-06-11 13:31:04 +0200

    Fixes #23 : Fallback to default configuration if no external is provided. (#68)

--- cassandra-diagnostics-core/src/test/java/io/smartcat/cassandra/diagnostics/config/YamlConfigurationLoaderTest.java
+++ cassandra-diagnostics-core/src/test/java/io/smartcat/cassandra/diagnostics/config/YamlConfigurationLoaderTest.java
@@ -1,7 +1,6 @@
 package io.smartcat.cassandra.diagnostics.config;
 
-import static org.junit.Assert.assertNotNull;
-
+import static org.assertj.core.api.Assertions.*;
 import org.junit.Test;
 
 /**
@@ -14,7 +13,40 @@ public class YamlConfigurationLoaderTest {
     public void loads_default_configuration() throws ConfigurationException {
         YamlConfigurationLoader loader = new YamlConfigurationLoader();
         Configuration configuration = loader.loadConfig();
-        assertNotNull(configuration);
+        assertThat(configuration).isNotNull();
     }
 
+    @Test
+    public void load_invalid_external_configuratio_uri() {
+        System.setProperty("cassandra.diagnostics.config", "invalid-cassandra-diagnostics-path.yml");
+        YamlConfigurationLoader loader = new YamlConfigurationLoader();
+        ConfigurationException exception = null;
+        try {
+            loader.loadConfig();
+        } catch (ConfigurationException e) {
+            exception = e;
+        }
+        assertThat(exception).isNotNull();        
+    }
+
+    @Test
+    public void load_invalid_external_configuratio() {
+        System.setProperty("cassandra.diagnostics.config", "invalid-cassandra-diagnostics.yml");
+        YamlConfigurationLoader loader = new YamlConfigurationLoader();
+        ConfigurationException exception = null;
+        try {
+            loader.loadConfig();
+        } catch (ConfigurationException e) {
+            exception = e;
+        }
+        assertThat(exception).isNotNull();        
+    }
+
+    @Test
+    public void load_external_valid_configuration() throws ConfigurationException {
+        System.setProperty("cassandra.diagnostics.config", "valid-cassandra-diagnostics.yml");
+        YamlConfigurationLoader loader = new YamlConfigurationLoader();
+        Configuration configuration = loader.loadConfig();
+        assertThat(configuration).isNotNull();        
+    }
 }

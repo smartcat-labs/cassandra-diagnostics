d82bac72ad24c3602d32442790947dd31d56c324
mgobec <matija0204@gmail.com>
2016-08-30 17:21:50 +0200

    Use higher period value

--- cassandra-diagnostics-core/src/test/java/io/smartcat/cassandra/diagnostics/module/requestrate/RequestRateModuleTest.java
+++ cassandra-diagnostics-core/src/test/java/io/smartcat/cassandra/diagnostics/module/requestrate/RequestRateModuleTest.java
@@ -23,7 +23,7 @@ public class RequestRateModuleTest {
 
     @Test
     public void should_load_default_configuration_and_initialize() throws ConfigurationException {
-        final RequestRateModule module = new RequestRateModule(testConfiguration(), testReporters());
+        final RequestRateModule module = new RequestRateModule(testConfiguration(1), testReporters());
         module.stop();
     }
 
@@ -37,7 +37,7 @@ public class RequestRateModuleTest {
             }
         };
 
-        final RequestRateModule module = new RequestRateModule(testConfiguration(), reporters);
+        final RequestRateModule module = new RequestRateModule(testConfiguration(1), reporters);
         boolean wait = latch.await(100, TimeUnit.MILLISECONDS);
         module.stop();
         assertThat(wait).isTrue();
@@ -57,19 +57,19 @@ public class RequestRateModuleTest {
         when(selectQuery.statementType()).thenReturn(Query.StatementType.SELECT);
         final Query updateQuery = mock(Query.class);
         when(updateQuery.statementType()).thenReturn(Query.StatementType.UPDATE);
-        final RequestRateModule module = new RequestRateModule(testConfiguration(), reporters);
-        for (int i = 0; i < 20; i++) {
+        final RequestRateModule module = new RequestRateModule(testConfiguration(3), reporters);
+        for (int i = 0; i < 100; i++) {
             module.process(selectQuery);
             module.process(updateQuery);
         }
-        boolean wait = latch.await(1100, TimeUnit.MILLISECONDS);
+        boolean wait = latch.await(3100, TimeUnit.MILLISECONDS);
 
         assertThat(wait).isTrue();
         module.stop();
 
         assertThat(testReporter.reported).hasSize(4);
-        assertThat(testReporter.reported.get(2).value()).isEqualTo(20.0);
-        assertThat(testReporter.reported.get(3).value()).isEqualTo(20.0);
+        assertThat(testReporter.reported.get(2).value()).isEqualTo(100.0);
+        assertThat(testReporter.reported.get(3).value()).isEqualTo(100.0);
     }
 
     @Test
@@ -83,17 +83,17 @@ public class RequestRateModuleTest {
             }
         };
 
-        final RequestRateModule module = new RequestRateModule(testConfiguration(), reporters);
+        final RequestRateModule module = new RequestRateModule(testConfiguration(1), reporters);
         boolean wait = latch.await(200, TimeUnit.MILLISECONDS);
         module.stop();
         assertThat(wait).isTrue();
     }
 
-    private ModuleConfiguration testConfiguration() {
+    private ModuleConfiguration testConfiguration(int period) {
         final ModuleConfiguration configuration = new ModuleConfiguration();
         configuration.measurement = "test_measurement";
         configuration.module = "io.smartcat.cassandra.diagnostics.module.requestrate.RequestRateModule";
-        configuration.options.put("period", 1);
+        configuration.options.put("period", period);
         configuration.options.put("timeunit", "SECONDS");
         return configuration;
     }

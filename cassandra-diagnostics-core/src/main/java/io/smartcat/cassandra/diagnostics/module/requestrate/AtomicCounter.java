da9b65837af92b50b51a86456162b6290dacf1fe
Matija Gobec <matija0204@gmail.com>
2016-10-03 10:08:46 +0200

    Moving to AtomicLong for request rate module (#116)

---
+++ cassandra-diagnostics-core/src/main/java/io/smartcat/cassandra/diagnostics/module/requestrate/AtomicCounter.java
@@ -0,0 +1,35 @@
+package io.smartcat.cassandra.diagnostics.module.requestrate;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+/**
+ * Atomic counter implementation.
+ */
+public class AtomicCounter {
+
+    private AtomicLong counter = new AtomicLong();
+
+    /**
+     * Increment value by {@code 1}.
+     */
+    public void increment() {
+        counter.incrementAndGet();
+    }
+
+    /**
+     * Decrement value by {@code 1}.
+     */
+    public void decrement() {
+        counter.decrementAndGet();
+    }
+
+    /**
+     * Get sum then reset counter to {@code 0}.
+     *
+     * @return Returns sum.
+     */
+    public long sumThenReset() {
+        return counter.getAndSet(0);
+    }
+
+}

84921aebc2b7b77b25e9e6012a2185d592de6530
Nenad Bozic <nbozicns@gmail.com>
2016-12-26 17:20:16 +0100

    Fixing typo.

--- cassandra-diagnostics-connector30/src/test/java/io/smartcat/cassandra/diagnostics/connector/Connector30QueryReporterTest.java
+++ cassandra-diagnostics-connector30/src/test/java/io/smartcat/cassandra/diagnostics/connector/Connector30QueryReporterTest.java
@@ -79,7 +79,7 @@ public class Connector30QueryReporterTest {
             }
         };
         ConnectorConfiguration configuration = new ConnectorConfiguration();
-        Connector30QueryReporter connector21QueryReporter = new Connector30QueryReporter(queryReporter, configuration);
+        Connector30QueryReporter connector30QueryReporter = new Connector30QueryReporter(queryReporter, configuration);
 
         SelectStatement statement = mock(SelectStatement.class);
         String selectStatement = "SELECT * FROM test_keyspace.test_table;";
@@ -92,7 +92,7 @@ public class Connector30QueryReporterTest {
         setFinal(clientState, clientState.getClass().getDeclaredField("isInternal"), true);
         when(queryState.getClientState()).thenReturn(clientState);
 
-        connector21QueryReporter.report(System.currentTimeMillis(), System.currentTimeMillis(), statement,
+        connector30QueryReporter.report(System.currentTimeMillis(), System.currentTimeMillis(), statement,
                 selectStatement, queryState);
         lock.await(1000, TimeUnit.MILLISECONDS);
 

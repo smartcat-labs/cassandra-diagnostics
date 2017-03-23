abc9d11f073f8eafcf3f9aea141ee83f2b572095
Nebojsa Arezina <nebojsa.arezina@smartcat.io>
2017-03-23 14:38:54 +0100

    Add bash-oriented cassandra-diagnostics installer for Linux

---
+++ bash-installer/installer/non-interactive-installer.sh
@@ -0,0 +1,69 @@
+#!/usr/bin/env bash
+
+# Non-interactive cassandra-diagnostics installer.
+
+FILE_COPYING_ERROR_EXIT_CODE=1
+
+function print_usage() {
+    echo "Usage: $1 [OPTION]..."
+    echo "Automates installation of cassandra-diagnostics into existing Cassandra"
+    echo "installation."
+    echo ""
+    echo "Mandatory arguments:"
+    echo "-c, --cassandra-conf-dir               location of Cassandra configuration"
+    echo "                                       directory"
+    echo "-l, --cassandra-lib-dir                location of Cassandra library directory"
+    echo "-v, --cassandra-version                installed Cassandra version number "
+    echo "                                       (e.g 3.0.12, 2.1.4, etc.)"
+    echo "-V, --cassandra-diagnostics-version    desired version of cassandra-diagnostics"
+    echo "-C, --cassandra-diagnostics-conf-file  location of cassandra-diagnostics"
+    echo "                                       configuration (YAML) file"
+    echo "-h, --help                             show this message"
+    echo ""
+    echo "cassandra-diagnostics source code and documentation URL:"
+    echo "<https://github.com/smartcat-labs/cassandra-diagnostics>"
+}
+
+# Show usage if there are no input parameters.
+if [ $# == 0 ]; then
+    print_usage "$INSTALLER_SCRIPT_NAME"
+    exit 0
+fi
+
+# Include arguments parser functions.
+. "$PAYLOAD_DIRECTORY"/input-arguments-parser.sh
+
+# Parse arguments and create global variables used by installer.
+parse_input_arguments "$@"
+
+# Show usage if help input parameter is passed.
+if [ $INSTALLER_SHOW_USAGE -eq 1 ]; then
+    print_usage "$INSTALLER_SCRIPT_NAME"
+    exit 0
+fi
+
+# Include arguments validation functions.
+. "$PAYLOAD_DIRECTORY"/input-arguments-validator.sh
+
+# Validate input arguments.
+validate_input_arguments
+
+# Include scripts that depend on properly set global variables.
+. "$PAYLOAD_DIRECTORY"/diagnostic-library-files-manager.sh
+. "$PAYLOAD_DIRECTORY"/cassandra-diagnostics-urls.sh
+. "$PAYLOAD_DIRECTORY"/cassandra-diagnostics-jvm-opts-manager.sh
+
+REPORTER_MODULES=$(find_reporters_in "$CASSANDRA_DIAGNOSTICS_CONF_FILE")
+
+print_info "Removing existing cassandra-diagnostics libraries."
+remove_installed_cassandra_diagnostics_libraries
+
+print_info "Downloading cassandra-diagnostics libraries to $CASSANDRA_LIB_DIR:"
+download_diagnostics_libraries
+print_info "Done downloading."
+
+print_info "Removing old diagnostics configuration from $CASSANDRA_ENV_SCRIPT_NAME, if present."
+remove_diagnostics_configuration_from_env_script
+
+print_info "Appending cassandra-diagnostics configuration to $CASSANDRA_ENV_SCRIPT_NAME."
+append_diagnostics_configuration_to_env_script
---
+++ bash-installer/installer/prerequisites-checker.sh
@@ -0,0 +1,70 @@
+#!/usr/bin/env bash
+
+# Contains functions for checking prerequisites for running installer.
+
+UNSUPPORTED_BASH_VERSION_EXIT_CODE=20
+UNSUPPORTED_GETOPT_VERSION_EXIT_CODE=21
+UNSUPPORTED_AWK_VERSION_EXIT_CODE=22
+MISSING_BINARY_EXIT_CODE=23
+
+# Checks if bash version is 4 or newer.
+#
+# Exits:
+#   - with $UNSUPPORTED_BASH_VERSION_EXIT_CODE, if bash version is less than 4.
+function check_bash_version() {
+    local bash_version=$(bash --version | head -1 | cut -d " " -f 4)
+
+    if ! startswith "4." "$bash_version"; then
+        print_error "Unsupported bash version: $bash_version. Version 4 or newer is required."
+
+        exit $UNSUPPORTED_BASH_VERSION_EXIT_CODE
+    fi
+}
+
+# Checks if getopt is an enhanced version.
+#
+# Exits:
+#   - with $UNSUPPORTED_GETOPT_VERSION_EXIT_CODE, if getopt is not an enhanced version.
+function check_getopt_version() {
+    getopt --test >/dev/null 2>&1
+
+    if [ $? != 4 ]; then
+        print_error "Unsupported getopt version. Enhanced version is required."
+    fi
+}
+
+# Checks if Awk installation is GNU Awk 4 or newer.
+#
+# Exits:
+#   - with $UNSUPPORTED_AWK_VERSION_EXIT_CODE, if Awk version is not supported.
+function check_awk_version() {
+    local awk_version=$(awk --version | head -1 | cut -d "," -f 1)
+
+    if ! startswith "GNU Awk 4." "$awk_version"; then
+        print_error "Unsupported Awk version: $bash_version. GNU Awk version 4 or newer is required."
+
+        exit $UNSUPPORTED_AWK_VERSION_EXIT_CODE
+    fi
+}
+
+# Checks if binary can be found on $PATH.
+#
+# Exits:
+#   - with $MISSING_BINARY_EXIT_CODE, if binary is not on $PATH.
+function check_if_binary_is_in_path() {
+    which "$1" >/dev/null 2>&1
+
+    if [ $? != 0 ]; then
+        print_error "Cannot find $1 in PATH. Please install it to be able to use installer."
+
+        exit $MISSING_BINARY_EXIT_CODE
+    fi
+}
+
+function check_prerequisites() {
+    check_bash_version
+    check_getopt_version
+    check_awk_version
+    check_if_binary_is_in_path "wget"
+    check_if_binary_is_in_path "readlink"
+}

794a2c9f8ed00f8921f94ef5920ef1154e5d9d69
Nenad Bozic <nbozicns@gmail.com>
2017-04-23 18:04:07 +0200

    Using mock to verify Datadog reporter behavior.

--- cassandra-diagnostics-reporter-datadog/src/test/java/io/smartcat/diagnostics/reporter/DatadogReporterTest.java
+++ cassandra-diagnostics-reporter-datadog/src/test/java/io/smartcat/diagnostics/reporter/DatadogReporterTest.java
@@ -1,20 +1,22 @@
 package io.smartcat.diagnostics.reporter;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Matchers.anyDouble;
+import static org.mockito.Matchers.anyString;
+import static org.mockito.Matchers.eq;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 
-import java.io.IOException;
-import java.net.DatagramPacket;
-import java.net.DatagramSocket;
-import java.net.SocketException;
-import java.net.UnknownHostException;
-import java.util.ArrayList;
+import java.lang.reflect.Field;
 import java.util.Arrays;
 import java.util.LinkedHashMap;
-import java.util.List;
 import java.util.Map;
 import java.util.concurrent.TimeUnit;
 
+import org.junit.Before;
 import org.junit.Test;
+
+import com.timgroup.statsd.StatsDClient;
 
 import io.smartcat.cassandra.diagnostics.GlobalConfiguration;
 import io.smartcat.cassandra.diagnostics.Measurement;
@@ -23,8 +25,24 @@ import io.smartcat.cassandra.diagnostics.reporter.ReporterConfiguration;
 
 public class DatadogReporterTest {
 
+    private DatadogReporter datadogReporterWithMockClient;
+    private StatsDClient mockClient;
+
+    @Before
+    public void initialize() throws NoSuchFieldException, SecurityException, Exception {
+        final ReporterConfiguration config = new ReporterConfiguration();
+        config.options.put("statsDHost", "localhost");
+        config.options.put("statsDPort", 9876);
+        config.options.put("keysPrefix", "prefix");
+        config.options.put("fixedTags", Arrays.asList("host:somehost,tag2:two,tag3:three"));
+        datadogReporterWithMockClient = new DatadogReporter(config, GlobalConfiguration.getDefault());
+        mockClient = mock(StatsDClient.class);
+        setField(datadogReporterWithMockClient, datadogReporterWithMockClient.getClass().getDeclaredField("client"),
+                mockClient);
+    }
+
     @Test
-    public void should_load_configuration() {
+    public void should_load_configuration() throws Exception {
         final ReporterConfiguration config = new ReporterConfiguration();
         config.options.put("statsDHost", "localhost");
         config.options.put("statsDPort", 7000);
@@ -61,18 +79,7 @@ public class DatadogReporterTest {
     }
 
     @Test
-    public void should_send_measurement() throws SocketException, InterruptedException, UnknownHostException {
-        final TestUDPServer testUDPServer = new TestUDPServer(1);
-        Thread runner = new Thread(testUDPServer);
-        runner.start();
-
-        final ReporterConfiguration config = new ReporterConfiguration();
-        config.options.put("statsDHost", "localhost");
-        config.options.put("statsDPort", 9876);
-        config.options.put("keysPrefix", "prefix");
-        config.options.put("fixedTags", Arrays.asList("host:somehost,tag2:two,tag3:three"));
-        final DatadogReporter reporter = new DatadogReporter(config, GlobalConfiguration.getDefault());
-
+    public void should_send_measurement() throws Exception {
         Map<String, String> tags = new LinkedHashMap<>();
         tags.put("tag1", "tv1");
         tags.put("tag2", "tv2");
@@ -83,31 +90,14 @@ public class DatadogReporterTest {
         final Measurement measurement = Measurement
                 .create("test-metric", 909.0, System.currentTimeMillis(), TimeUnit.MILLISECONDS, tags, fields);
 
-        reporter.report(measurement);
+        datadogReporterWithMockClient.report(measurement);
 
-        runner.join(500);
-
-        assertThat(testUDPServer.receivedMessages.size()).isEqualTo(1);
-        assertThat(testUDPServer.receivedMessages.get(0))
-                .contains("test-metric:909|g|#host:somehost,tag2:two,tag3:three,tag2:tv2,tag1:tv1");
-
-        testUDPServer.stop();
+        verify(mockClient).recordGaugeValue(eq(measurement.name()), eq(measurement.getValue()), eq("tag1:tv1"),
+                eq("tag2:tv2"));
     }
 
     @Test
-    public void should_send_measurement_for_each_field_in_complex_measurement()
-            throws SocketException, InterruptedException, UnknownHostException {
-        final TestUDPServer testUDPServer = new TestUDPServer(4);
-        Thread runner = new Thread(testUDPServer);
-        runner.start();
-
-        final ReporterConfiguration config = new ReporterConfiguration();
-        config.options.put("statsDHost", "localhost");
-        config.options.put("statsDPort", 9876);
-        config.options.put("keysPrefix", "prefix");
-        config.options.put("fixedTags", Arrays.asList("host:somehost,tag2:two,tag3:three"));
-        final DatadogReporter reporter = new DatadogReporter(config, GlobalConfiguration.getDefault());
-
+    public void should_send_measurement_for_each_field_in_complex_measurement() throws Exception {
         Map<String, String> tags = new LinkedHashMap<>();
         tags.put("tag1", "tv1");
         tags.put("tag2", "tv2");
@@ -121,37 +111,13 @@ public class DatadogReporterTest {
         final Measurement measurement = Measurement
                 .create("test-metric", null, System.currentTimeMillis(), TimeUnit.MILLISECONDS, tags, fields);
 
-        reporter.report(measurement);
-        runner.join(500);
+        datadogReporterWithMockClient.report(measurement);
 
-        assertThat(testUDPServer.receivedMessages.size()).isEqualTo(4);
-
-        assertThat(testUDPServer.receivedMessages.get(0))
-                .isEqualTo("prefix.test-metric.v1:15|g|#host:somehost,tag2:two,tag3:three,tag2:tv2,tag1:tv1");
-        assertThat(testUDPServer.receivedMessages.get(1))
-                .isEqualTo("prefix.test-metric.v2:26|g|#host:somehost,tag2:two,tag3:three,tag2:tv2,tag1:tv1");
-        assertThat(testUDPServer.receivedMessages.get(2))
-                .isEqualTo("prefix.test-metric.v3:50|g|#host:somehost,tag2:two,tag3:three,tag2:tv2,tag1:tv1");
-        assertThat(testUDPServer.receivedMessages.get(3))
-                .isEqualTo("prefix.test-metric.v4:1234|g|#host:somehost,tag2:two,tag3:three,tag2:tv2,tag1:tv1");
-
-        testUDPServer.stop();
+        verify(mockClient, times(4)).recordGaugeValue(anyString(), anyDouble(), anyString(), anyString());
     }
 
     @Test
-    public void should_skip_non_number_fields_in_complex_measurement()
-            throws SocketException, InterruptedException, UnknownHostException {
-        final TestUDPServer testUDPServer = new TestUDPServer(4);
-        Thread runner = new Thread(testUDPServer);
-        runner.start();
-
-        final ReporterConfiguration config = new ReporterConfiguration();
-        config.options.put("statsDHost", "localhost");
-        config.options.put("statsDPort", 9876);
-        config.options.put("keysPrefix", "prefix");
-        config.options.put("fixedTags", Arrays.asList("host:somehost,tag2:two,tag3:three"));
-        final DatadogReporter reporter = new DatadogReporter(config, GlobalConfiguration.getDefault());
-
+    public void should_skip_non_number_fields_in_complex_measurement() throws Exception {
         Map<String, String> tags = new LinkedHashMap<>();
         tags.put("tag1", "tv1");
         tags.put("tag2", "tv2");
@@ -166,53 +132,14 @@ public class DatadogReporterTest {
         final Measurement measurement = Measurement
                 .create("test-metric", null, System.currentTimeMillis(), TimeUnit.MILLISECONDS, tags, fields);
 
-        reporter.report(measurement);
-        runner.join(500);
+        datadogReporterWithMockClient.report(measurement);
 
-        assertThat(testUDPServer.receivedMessages.size()).isEqualTo(1);
-        assertThat(testUDPServer.receivedMessages.get(0))
-                .isEqualTo("prefix.test-metric.v4:1234|g|#host:somehost,tag2:two,tag3:three,tag2:tv2,tag1:tv1");
-
-        testUDPServer.stop();
+        verify(mockClient).recordGaugeValue(eq(measurement.name() + ".v4"),
+                eq(Double.parseDouble(measurement.fields().get("v4"))), eq("tag1:tv1"), eq("tag2:tv2"));
     }
 
-    private class TestUDPServer implements Runnable {
-
-        private DatagramSocket serverSocket = new DatagramSocket(9876);
-
-        private byte[] receiveData = new byte[1024];
-
-        private DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
-
-        private final int expectedMessages;
-
-        private TestUDPServer(int expectedMessages) throws SocketException, UnknownHostException {
-            this.expectedMessages = expectedMessages;
-        }
-
-        public List<String> receivedMessages = new ArrayList<>();
-
-        public void run() {
-            try {
-                while (true) {
-                    serverSocket.receive(receivePacket);
-                    String[] messages = new String(receivePacket.getData()).split("\n");
-                    for (String message : messages) {
-                        receivedMessages.add(message.trim());
-                    }
-                    if (receivedMessages.size() == this.expectedMessages) {
-                        break;
-                    }
-                }
-            } catch (IOException e) {
-                assert false;
-            }
-        }
-
-        public void stop() {
-            serverSocket.close();
-            serverSocket = null;
-        }
+    private void setField(Object target, Field field, Object newValue) throws Exception {
+        field.setAccessible(true);
+        field.set(target, newValue);
     }
-
 }

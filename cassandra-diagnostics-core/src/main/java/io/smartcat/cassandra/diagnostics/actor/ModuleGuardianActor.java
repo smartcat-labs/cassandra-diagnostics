539a8a157895ddeea52b6c2a8b0816293bc4dd19
mgobec <matija0204@gmail.com>
2017-05-31 09:14:51 +0200

    Actor creation

--- cassandra-diagnostics-core/src/main/java/io/smartcat/cassandra/diagnostics/actor/ModuleGuardianActor.java
+++ cassandra-diagnostics-core/src/main/java/io/smartcat/cassandra/diagnostics/actor/ModuleGuardianActor.java
@@ -1,9 +1,12 @@
 package io.smartcat.cassandra.diagnostics.actor;
 
+import akka.actor.ActorRef;
 import akka.event.Logging;
 import akka.event.LoggingAdapter;
+import akka.routing.ActorRefRoutee;
 import akka.routing.BroadcastRoutingLogic;
 import akka.routing.Router;
+import io.smartcat.cassandra.diagnostics.config.Configuration;
 
 /**
  * Module actor guardian. Creates all module actors defined in configuration and takes care of their behavior and
@@ -17,6 +20,22 @@ public class ModuleGuardianActor extends BaseActor {
 
     @Override
     public Receive createReceive() {
-        return receiveBuilder().build();
+        return receiveBuilder()
+                .match(Configuration.class, this::configure)
+                .build();
+    }
+
+    private void configure(final Configuration configuration) {
+        configuration.modules.stream().forEach((moduleConfiguration) -> {
+            try {
+                ActorRef module = getContext().actorOf(ActorFactory.props(moduleConfiguration.module));
+                module.tell(moduleConfiguration, self());
+
+                getContext().watch(module);
+                router.addRoutee(new ActorRefRoutee(module));
+            } catch (Exception e) {
+                logger.warning("Failed to create module by class name " + moduleConfiguration.module, e);
+            }
+        });
     }
 }
--- cassandra-diagnostics-core/src/main/java/io/smartcat/cassandra/diagnostics/actor/ReporterGuardianActor.java
+++ cassandra-diagnostics-core/src/main/java/io/smartcat/cassandra/diagnostics/actor/ReporterGuardianActor.java
@@ -1,10 +1,13 @@
 package io.smartcat.cassandra.diagnostics.actor;
 
+import akka.actor.ActorRef;
 import akka.event.Logging;
 import akka.event.LoggingAdapter;
+import akka.routing.ActorRefRoutee;
 import akka.routing.BroadcastRoutingLogic;
 import akka.routing.Router;
 import io.smartcat.cassandra.diagnostics.Measurement;
+import io.smartcat.cassandra.diagnostics.config.Configuration;
 
 /**
  * Reporter actor guardian. Creates all reporter actors defined in configuration and takes care of their behavior and
@@ -18,15 +21,24 @@ public class ReporterGuardianActor extends BaseActor {
 
     @Override
     public Receive createReceive() {
-        return receiveBuilder().match(Measurement.class, this::routeMeasurement).build();
+        return receiveBuilder()
+                .match(Measurement.class, this::routeMeasurement)
+                .match(Configuration.class, this::configure)
+                .build();
     }
 
-    private void configure() {
-//        config.reporters.stream().forEach((reporterConfig -> {
-//        ActorRef reporter = getContext().actorOf(ActorFactory.props(reporterConfig.name));
-//        getContext().watch(reporter);
-//        router.addRoutee(new ActorRefRoutee(reporter));
-//        }));
+    private void configure(final Configuration configuration) {
+        configuration.reporters.stream().forEach((reporterConfig) -> {
+            try {
+                ActorRef reporter = getContext().actorOf(ActorFactory.props(reporterConfig.reporter));
+                reporter.tell(reporterConfig, self());
+
+                getContext().watch(reporter);
+                router.addRoutee(new ActorRefRoutee(reporter));
+            } catch (Exception e) {
+                logger.warning("Failed to create reporter by class name " + reporterConfig.reporter, e);
+            }
+        });
     }
 
     private void routeMeasurement(final Measurement measurement) {
